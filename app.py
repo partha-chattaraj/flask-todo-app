f1d4cb2be246c89ede11a8a016075be2164ee208
Partha <parthachattaraj23@gnail.com>
2026-03-19 02:03:37 +0530

    todo app project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        task = request.form.get("task")
+        tasks.append(task)
+        return redirect("/")
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    tasks.pop(id)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
